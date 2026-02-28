ab34921c99b86c21f1dc7776d6996aec00cc63e8
Stephan Hügel <shugel@tcd.ie>
2026-02-28 12:00:11 +0000

    Add SQLite PRAGMAs: synchronous=NORMAL, busy_timeout, temp_store=memory

--- src/bsearch/db.py
+++ src/bsearch/db.py
@@ -81,6 +81,9 @@ class Database:
         conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA journal_mode=WAL")
         conn.execute("PRAGMA foreign_keys=ON")
+        conn.execute("PRAGMA synchronous=NORMAL")
+        conn.execute("PRAGMA busy_timeout=5000")
+        conn.execute("PRAGMA temp_store=memory")
         return conn
 
     def _init_schema(self) -> None:
